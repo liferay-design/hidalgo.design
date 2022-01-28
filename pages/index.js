2333e4639ebe69b05aa6304a61200c0ccc7fdda9
plhnk <paul.hanaoka@gmail.com>
2022-01-28 02:30:12 +0100

    create index, bring content in md

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// /** @jsx jsx */
+
+import { jsx } from 'theme-ui'
+import ColorSwitcher from '../components/color-switcher'
+
+export default () => {
+  return (
+    <div>
+      <ColorSwitcher />
+      hello world
+    </div>
+  )
+}
